c2e304ea96ac5518d66a29c379d6a5cca5ce39a9
Oleh Chyhyryn <oleh.chyhyryn@outlook.com>
2023-07-25 15:19:07 +0300

    review

--- src/ilcdlib/epd/dialect/environdec.py
+++ src/ilcdlib/epd/dialect/environdec.py
@@ -32,13 +32,16 @@ class EnvirondecIlcdXmlEpdReader(IlcdEpdReader):
     """Reader for EPDs in the Environdec specific ILCD XML format."""
 
     _TIME_REPR_DESC_DELIMITER = "\r\n"
+    _ENVIRONDEC_DETAIL_URL_V1 = re.compile(r"https://www.environdec.com/library/_\?Epd=\d+")
+    _ENVIRONDEC_HTML_FRIENDLY_URL = re.compile(r'"friendlyUrl": ?"(epd\d+)"')
+    _ENVIRONDEC_DETAIL_URL_v2 = re.compile(r"https://www.environdec.com/Detail/epd\d+")
 
     def get_epd_document_stream(self) -> IO[bytes] | None:
         """
         Download the EPD document from the Environdec API, if it is possible.
 
         In the case of Environdec EPD, the EPD document is not stored in the ILCD XML file, but is retrieved from the
-        Environdec their API and public EPD library.
+        Environdec API and public EPD library.
 
         We will not use this method to get the EPD document link for OpenEPD,
         because it is a responsibility of the client to get related documents.
@@ -47,13 +50,34 @@ class EnvirondecIlcdXmlEpdReader(IlcdEpdReader):
         link = self.get_url_attachment("en")
         if not link:
             return None
-        if not re.match(r"https://www.environdec.com/Detail/epd\d+", link):
+
+        if re.match(self._ENVIRONDEC_DETAIL_URL_V1, link):
+            # If we have an older url to product page, we need to parse the html response to get the friendly url
+            # For example, https://www.environdec.com/library/_?Epd=14879
+            response = requests.get(link)
+            if not response.status_code == 200:
+                return None
+            foreign_ids = re.findall(self._ENVIRONDEC_HTML_FRIENDLY_URL, response.text)
+            if not foreign_ids:
+                return None
+            foreign_id = foreign_ids[0]
+
+        elif re.match(self._ENVIRONDEC_DETAIL_URL_v2, link):
+            # If we have a newer url to product page, we can get it using url itself
+            # For example, https://www.environdec.com/library/epd1452
+            foreign_id = link.strip("/").split("/")[-1]
+        else:
+            # Otherwise, we can't get the foreign id
             return None
-        foreign_id = link.split("/")[-1]
+
         response = requests.get(f"https://api.environdec.com/api/v1/EPDLibrary/EPD/{foreign_id}")
+        if response.status_code != 200:
+            return None
         document_id = response.json()["documents"][0]["id"]
         pdf_link = f"https://api.environdec.com/api/v1/EPDLibrary/Files/{document_id}/Data"
         pdf_response = requests.get(pdf_link)
+        if pdf_response.status_code != 200:
+            return None
         return io.BytesIO(pdf_response.content)
 
     def _get_url_attachments_v1(self, lang: LangDef) -> str | None:
